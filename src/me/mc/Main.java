17159931c108e69549e0203694b63a50ef3a0840
jnfrle <jennifer.lee1@montgomerycollege.edu>
2019-09-09 20:42:19 -0400

    initial commit

---
+++ src/me/mc/Main.java
@@ -0,0 +1,15 @@
+package me.mc;
+
+public class Main {
+
+
+    public static void main(String[] args){
+        // access modifier
+        // available in memory
+        // return data type
+        // argument
+
+        System.out.println("Hello World! Love from Virginia");
+
+    }
+}
